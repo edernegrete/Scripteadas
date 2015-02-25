3eaad08cbfe63848c98689c722e0c5736a114ecd
Ederyairr <ederyairnglz@gmail.com>
2015-02-24 19:06:46 -0600

    Fix bugs

--- DominosCupones.js
+++ DominosCupones.js
@@ -1,8 +1,13 @@
-var letraCupon = 'A'
+var cuponLetter = ['A', 'B', 'C', 'D']
 var trigger =  $('.ui-btn.ui-input-btn.ui-btn-c.ui-corner-all.ui-shadow.ui-btn-inline.ui-mini.ui-icon-check.ui-btn-icon-left > input');
 
-for (var i = 0; i<=100; i++){	
-	$('#couponCode_a').val(letraCupon+i)
- 	$(trigger).click();
+function submitCode (letter) {
+	for (var i = 0; i<=100; i++){	
+		$('#couponCode_a').val(letter+i)
+	 	$(trigger).click();
+	}
 }
 
+cuponLetter.forEach(function(letter){
+   submitCode(letter);
+});
